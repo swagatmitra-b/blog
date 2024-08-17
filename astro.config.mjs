3e1395e7463c2cc64ac446409aa18090470e8a2e
Swagat <swagatmitrabhattacharya18@gmail.com>
2024-08-17 18:49:11 +0530

    newwwww post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
@@ -18,4 +20,8 @@ export default defineConfig({
     sitemap(),
     tailwind(),
   ],
+  markdown: {
+    rehypePlugins: [rehypeKatex],
+    remarkPlugins: [remarkMath],
+  },
 });
